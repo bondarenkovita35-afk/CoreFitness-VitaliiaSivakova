2fc0d4af50a2a144a279319fb486d4f1f1b0cacd
Vita <bondarenkovita35@gmail.com>
2026-03-26 15:25:01 +0100

    Add public pages, auth views, shared layout and local images

---
+++ Presentation.WebApp/Controllers/AuthController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class AuthController : Controller
+{
+    public IActionResult SignIn()
+    {
+        return View();
+    }
+
+    public IActionResult SignUp()
+    {
+        return View();
+    }
+
+    public IActionResult SetPassword()
+    {
+        return View();
+    }
+}
